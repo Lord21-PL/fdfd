915315bdfe3813c25d57c43a73fc35f1bbff4d95
Lord21-PL <35666380+Lord21-PL@users.noreply.github.com>
2023-10-14 03:52:14 +0200

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xab3EE173409605240a4CE85d80F372f46383c50F/g' config.conf
 sleep 5
 echo "---------------------------"
 
